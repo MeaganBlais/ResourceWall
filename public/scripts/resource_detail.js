12ee75c24aac67522fe28ff6c13afffd503cd4a3
tahuana <tahuana@gmail.com>
2017-06-22 23:17:34 +0000

    Getting the user information and updating the comment container.

--- public/scripts/resource_detail.js
+++ public/scripts/resource_detail.js
@@ -1,14 +1,25 @@
 $(document).ready( function() {
 
-  // Variable to get the resource id from html
+  // Variables to get information about the user and the resource id
   var resource_id = $('#url').data('id')
+  var user = JSON.parse(localStorage.getItem("userInfo"))
+  var user_id = user.id;
+  var user_name = user.user_name;
+  var user_avatar = user.avatar_URL;
+
+  //set the user information on comment container
+  $(".input_user").text(user_name);
+  $(".input_user").data("id", user_id);
+  $("#avatar_comment").attr("src", user_avatar);
+
+
 
   //function that takes in a comment object
   //and return a comment <article> element containing the entire HTML structure of the comment
-  function createCommentElement (CommentData) {
+  function createCommentElement (commentData) {
 
     //get how many days between today and the date when the comment was created
-    var dateCreated = new Date(CommentData.created_at);
+    var dateCreated = new Date(commentData.created_at);
     var dateToday = new Date();
 
     var timeDiff = Math.abs(dateToday.getTime() - dateCreated.getTime());
@@ -24,11 +35,11 @@ $(document).ready( function() {
     //create the structure of comment to be included in the html
     var comment = (`<article  class="comment_container">
                     <header>
-                      <img class="avatar" src ="${CommentData.avatar_URL}">
-                      <span class="input_user" data-id='${CommentData.user_id}'>${CommentData.user_name}</span>
+                      <img class="avatar" src ="${commentData.avatar_URL}">
+                      <span class="input_user" data-id='${commentData.user_id}'>${commentData.user_name}</span>
                     </header>
                     <div class="read-comment">
-                      <span>${CommentData.comment}</span>
+                      <span>${commentData.comment}</span>
                     </div>
                     <footer>
                       <span class="daysAgo">${diffDays}</span>
@@ -47,6 +58,10 @@ $(document).ready( function() {
     //define a variable to receive the html block for the comment
     var comment;
 
+    //clear the container before to read all comments
+    $("#tshow_comments_container").empty();
+
+
     //if there are any comment, loops through comments
     //calls createCommentElement for each comment and takes return value and appends it to the comments container
     if (comments.length > 0) {
@@ -58,14 +73,31 @@ $(document).ready( function() {
 
   };
 
-  // Get all comments for the resource
-  $.getJSON("/resources/" + resource_id + "/comments")
-        .done(function (resource) {
-                renderComments(resource);
-        })
-        .fail(function () {
-          console.log("error");
-        })
+  //
+  function updateCommentsCounter (comments) {
+
+    var totalOfComments = comments.length;
+
+    $("#totalOfComments").text(totalOfComments);
+
+  }
+
+  //function that load comments from db, and show on screen (call renderComments function)
+  function loadComments () {
+
+    // Get all comments for the resource
+    $.getJSON("/resources/" + resource_id + "/comments")
+      .done(function (resource) {
+              renderComments(resource);
+              updateCommentsCounter(resource);
+      })
+      .fail(function () {
+        console.log("error");
+      })
+  }
+
+  //start the page showing the comments
+  loadComments();
 
 })
 
