190380c1e4d68bfa47bbb8a2e46ed738d070fad4
Meagan Blais <meaganblais@gmail.com>
2017-06-22 17:30:49 -0400

    adding newResources & charCounter

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,16 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+$(document).ready(function() {
+  $('#new-resource').on('submit', function(event) {
+    event.preventDefault();
+
+    console.log('Button clicked, performing ajax call ...', $(this).serialize());
+    $.ajax({
+      url: '/api/resources',
+      method: 'POST',
+      data: $(this).serialize(),
+      success: function() {
+        console.log('Success: ');
+      }
+    });
+    $(this).find('.counter').html(255)
+  });
 });
