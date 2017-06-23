9640b32663378af6857fb81e10df0888ee12ef7e
tahuana <tahuana@gmail.com>
2017-06-23 16:31:51 +0000

    Updating the count char with the same function used in add resource page.

--- public/scripts/char-counter.js
+++ public/scripts/char-counter.js
@@ -7,12 +7,40 @@ function getRemChar(totChar) {
 }
 
 $(document).ready(() => {
+
   $('#new-resource').find('textarea').on('keyup keypress input', function(event) {
+
+    //Getting "this" to pass as parameter
+    var element = $(this);
+
+    //Calling a function to check the counter, given "this" as parameter
+    checkCounter(element);
+
+  });
+
+
+  $('#new-comment').find('textarea').on('keyup keypress input', function(event) {
+
+    //Getting "this" to pass as parameter
+    var element = $(this);
+
+    //Calling a function to check the counter, given "this" as parameter
+    checkCounter(element);
+
+  });
+
+  $('#new-resource').find('textarea').on('keyup keypress input', function(event) {
+  });
+
+
+  function checkCounter (element) {
+
     //wrapping 'this' in jQuery syntax necessary to use functions
-    charRemaining = getRemChar($(this).val().length);
+    charRemaining = getRemChar(element.val().length);
+
     // 'this' uses jQuery to traverse up the DOM tree
-    let counter = $(this).siblings('.counter');
-    
+    let counter = element.closest('form').find('.counter'); //CHANGE THIS
+
     counter.text(charRemaining);
 
     if (charRemaining < 0) {
@@ -20,5 +48,7 @@ $(document).ready(() => {
     } else {
       counter.removeClass('over-char-limit');
     }
-  });
+
+  }
+
 });
